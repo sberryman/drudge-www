4589df5e28104cadedeae39cb646366bc515fa9a
Shaun Berryman <shaun@shaunberryman.com>
2019-02-18 13:00:06 -0800

    Just getting started!

---
+++ public/javascripts/live.js
@@ -0,0 +1,59 @@
+// update the time ago every N seconds
+var updateTimeAgos = function () {
+  $('time').each(function (index) {
+    var timestamp = $(this).attr('datetime')
+    $(this).text(moment(parseInt(timestamp, 10)).fromNow())
+  })
+}
+setInterval(updateTimeAgos, 10000)
+
+// display a new article
+var newArticle = function (data) {
+  // render the article
+  $('#articles').prepend(`
+    <a href="${data.url}" class="list-group-item list-group-item-action" target="_blank">
+      ${data.text}
+      <small>
+        <time datetime="${data.at}"></time>
+        ${data.headline ? '<span class="badge badge-primary badge-pill">headline</span>' : ''}
+        ${data.important ? '<span class="badge badge-danger badge-pill">important</span>' : ''}
+      </small>
+    </a>
+  `)
+
+  // update the last update timestamp
+  $('#lastUpdate').attr('datetime', data.at.toString())
+
+  // trigger a timestamp update
+  updateTimeAgos()
+}
+
+function stringFromUTF8Array (data) {
+  const extraByteMap = [ 1, 1, 1, 1, 2, 2, 3, 0 ]
+  var count = data.length
+  var str = ''
+
+  for (var index = 0; index < count;) {
+    var ch = data[index++]
+    if (ch & 0x80) {
+      var extra = extraByteMap[(ch >> 3) & 0x07]
+      if (!(ch & 0x40) || !extra || ((index + extra) > count)) {
+        return null
+      }
+
+      ch = ch & (0x3F >> extra)
+      for (;extra > 0; extra -= 1) {
+        var chx = data[index++]
+        if ((chx & 0xC0) !== 0x80) {
+          return null
+        }
+
+        ch = (ch << 6) | (chx & 0x3F)
+      }
+    }
+
+    str += String.fromCharCode(ch)
+  }
+
+  return str
+}
---
+++ routes/index.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const router = express.Router()
+const sqlite3 = require('sqlite3').verbose()
+const moment = require('moment')
+const Joi = require('joi')
+const hbs = require('hbs')
+
+// validate it!
+const schema = Joi.object({
+  DB_PATH: Joi.string(),
+  MQTT_URL: Joi.string()
+    .uri({
+      scheme: ['mqtt', 'mqtts', 'ws', 'wss']
+    }),
+  MQTT_TOPIC: Joi.string()
+})
+const { error, value: config } = Joi.validate(
+  process.env,
+  schema,
+  {
+    presence: 'required',
+    stripUnknown: true
+  }
+)
+if (error) {
+  throw new Error(`Persist config validation error: ${error.message}`)
+}
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  // create the db connection
+  const db = new sqlite3.Database(config.DB_PATH)
+
+  db.all(
+    'SELECT * FROM links ORDER BY ts_first DESC LIMIT 25',
+    [],
+    (err, rows) => {
+      // close the db connection
+      db.close()
+      // console.log(rows)
+
+      // log the error
+      if (err) {
+        throw err
+      }
+
+      if (!rows || rows.length < 1) {
+        throw new Error('No news in the database!!!')
+      }
+
+      // force rows bit to boolean and rename ts_first to 'at'
+      const mappedRows = rows.map((item) => {
+        return {
+          at: item.ts_first,
+          important: item.important === 1,
+          headline: item.headline === 1,
+          text: item.summary,
+          url: item.url
+        }
+      })
+
+      res.render(
+        'index',
+        {
+          title: 'DrudgeReport - Realtime',
+          mqttUrl: config.MQTT_URL,
+          mqttTopic: config.MQTT_TOPIC,
+          articles: mappedRows,
+          lastUpdate: rows[0].ts_last
+        }
+      )
+    }
+  )
+})
+
+hbs.registerHelper('momentFromNow', (item, options) => {
+  return moment(item).fromNow()
+})
+
+module.exports = router
